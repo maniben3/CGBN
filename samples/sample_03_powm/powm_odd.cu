035769483cc16b36bcadfa51028d6efd6c481ae7
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-10 20:17:40 +0530

    Update powm_odd.cu

--- samples/sample_03_powm/powm_odd.cu
+++ samples/sample_03_powm/powm_odd.cu
@@ -346,7 +346,7 @@ void run_test(uint32_t instance_count) {
 int main() {
   uint_32 params;
   printf("Enter an integer: ");
-  scanf("%d", &params);
+  scanf("%u", &params);
   typedef powm_params_t<8, 1024, 5> params;
   run_test(params);
 }
