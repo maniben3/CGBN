c33db3bbe3c7cc3ecc1228cf6f092d2f8aba6db2
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-15 16:57:40 +0530

    Update add.cu

--- samples/sample_01_add/add.cu
+++ samples/sample_01_add/add.cu
@@ -108,11 +108,12 @@ __global__ void kernel_add(cgbn_error_report_t *report, instance_t *instances, u
 
   context_t      bn_context(cgbn_report_monitor, report, instance);   // construct a context
   env_t          bn_env(bn_context.env<env_t>());                     // construct an environment for 1024-bit math
-  env_t::cgbn_t  a, b, r;                                             // define a, b, r as 1024-bit bignums
+  env_t::cgbn_t  a, b, r,c;                                             // define a, b, r as 1024-bit bignums
 
   cgbn_load(bn_env, a, &(instances[instance].a));      // load my instance's a value
   cgbn_load(bn_env, b, &(instances[instance].b));      // load my instance's b value
-  cgbn_add(bn_env, r, a, b);                           // r=a+b
+  cgbn_add(bn_env, c, a, b);                           // r=a+b
+  cgbn_add(bn_env, r, c,b);     
   cgbn_store(bn_env, &(instances[instance].sum), r);   // store r into sum
 }
 
