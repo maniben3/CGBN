3fa88af3abfada1c765829f730edb15ea6107cf9
maniben3 <106887188+maniben3@users.noreply.github.com>
2023-01-16 08:03:08 +0530

    Update powm_odd.cu

--- samples/sample_03_powm/powm_odd.cu
+++ samples/sample_03_powm/powm_odd.cu
@@ -1,27 +1,3 @@
-/***
-
-Copyright (c) 2018-2019, NVIDIA CORPORATION.  All rights reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-
-***/
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -251,9 +227,9 @@ class powm_odd_t {
     int         index;
   
     for(index=0;index<count;index++) {
-      random_words(instances[index].x._limbs, params::BITS/32);
-      random_words(instances[index].power._limbs, params::BITS/32);
-      random_words(instances[index].modulus._limbs, params::BITS/32);
+      random_words_base(instances[index].x._limbs, params::BITS/32);
+      random_words_power(instances[index].power._limbs, params::BITS/32);
+      random_words_exp(instances[index].modulus._limbs, params::BITS/32);
 
       // ensure modulus is odd
       instances[index].modulus._limbs[0] |= 1;
@@ -382,6 +358,6 @@ void run_test(uint32_t instance_count) {
 
 int main() {
   typedef powm_params_t<8, 1024, 5> params;
-  run_test<params>(10);
+  run_test<params>(1);
   return 0;
 }
